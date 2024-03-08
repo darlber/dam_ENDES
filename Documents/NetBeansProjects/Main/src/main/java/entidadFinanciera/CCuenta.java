64bafc017739810becfb5c8f39140f50010bf2d1
Alberto <maui_195@hotmail.com>
2024-03-08 21:15:05 +0100

    Commit de CCuenta después del ejercicio 7 tras refactorizar e introducir parametros

---
+++ Documents/NetBeansProjects/Main/src/main/java/entidadFinanciera/CCuenta.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidadFinanciera;
+
+/**
+ *
+ * @author Alberto
+ */
+
+public class CCuenta {
+
+    /**
+     * Nombre de la persona titular
+     */
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+    final String ERRORCANTIDADNEGATIVA = "No se puede ingresar una cantidad negativa";
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+    
+    
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+    
+    public double estado(){
+        return this.saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (saldo < cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+}
+
+
+    
+   
